31787b4a9274bde7ff39776befd7f57e5130b8fc
jmarioasilva <jmarioasilva@gmail.com>
2024-06-30 22:33:22 -0300

    jmarioasilva

--- api/db.js
+++ api/db.js
@@ -1,9 +1,13 @@
 import { Sequelize } from "sequelize";
+import mysql2 from "mysql2";
 import 'dotenv/config'
+
+
 
 const Databases = new Sequelize(process.env.NAME_DATABASE, process.env.USER_DATABASE, process.env.PASSWORD_DATABASE, {
     host: process.env.HOST_DATABASE,
     dialect: process.env.TIPE_DATABASE,
+    dialectModule: mysql2,
     port: 3306
 });
 
